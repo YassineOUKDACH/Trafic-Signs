ad5b6f3b6b2bb41c9e1a268361d5cb38a33fb475
YasssineOUKDACH <59118937+YassineOUKDACH@users.noreply.github.com>
2021-03-06 16:46:23 +0100

    Delete deplyment.py

--- deplyment.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request
-#from scipy.misc import imsave, imread, imresize
-import numpy as np 
-import keras.models
-import re
-import sys
-import os
-sys.path.append(os.path.abspath('./Traffic sign classification'))
-from load import *
